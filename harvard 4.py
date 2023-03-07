52a31f61deafb616fd9301fdd145bc2896f9da77
Miguel Piffer <miguel-piffer1@hotmail.com>
2023-03-07 10:20:43 -0300

    atualização aula

---
+++ harvard 4.py
@@ -0,0 +1,14 @@
+score= int(input('pontuação'))
+if score >= 90 and score <=100:
+    print("você tirou um A")
+elif score >= 80 and score < 90: 
+    print("você tirou um B")
+elif score >= 70 and score < 80:
+    print("você tirou um C") 
+elif score >= 60 and score < 70:
+    print("você tirou um D")
+else :
+    print("você está abaixo da média")
+    
+
+               
