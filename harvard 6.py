b0a2c77b25abad0691e2ed87328d0f9f43f42867
Miguel Piffer <miguel-piffer1@hotmail.com>
2023-03-07 11:34:47 -0300

    atualização

---
+++ harvard 6.py
@@ -0,0 +1,20 @@
+def main():
+    x= int (input("o que é x?"))
+    if é_par(x):
+        print("par")
+    else:
+        print("ímpar")    
+def é_par(n):
+        if n % 2 == 0:
+             return True
+        else:
+             return False
+main()      
+        
+         
+    
+
+        
+    
+
+               
---
+++ harvard 7.py
@@ -0,0 +1,17 @@
+def main():
+    x= int (input("o que é x?"))
+    if é_par(x):
+        print("par")
+    else:
+        print("ímpar")    
+def é_par(n):
+        return n % 2 == 0
+main()      
+        
+
+    
+
+        
+    
+
+               
