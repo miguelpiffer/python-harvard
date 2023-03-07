4e5893387698ff3547a8704de1a616549db48327
miguelpiffer <126285004+miguelpiffer@users.noreply.github.com>
2023-03-07 11:42:50 -0300

    Delete je.py

--- je.py
+++
@@ -1,6 +0,0 @@
-n1=float(input('digite a primeira nota'))
-n2=float(input('digite a segunda nota'))
-n3=float(input('digite a terceira nota'))
-n4=float(input('digite a quarta nota'))
-s=(n1+n2+n3+n4)/4
-print(' a média aritimética das 4 notas é : {} !' .format(s))
